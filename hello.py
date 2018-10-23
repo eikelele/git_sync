9e02aef71c56685fb640887a0d815bc50d64b5da
eikelele <dereisa25@gmail.com>
2018-10-23 16:41:35 +0200

    added lists,tuples,dicts to resume

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
-print('Hello world')
+myString = 'Hello World'
+myList = [1,2,3]
+myTuple = (4,5,6)
+myDict = {'apple':1.99,'banana':0.99}
+print(f'This is my {myString} project.')
+print(f'It contains Lists: {myList}, Tuples:{myTuple} and Dictionaries with prices of apples amd bananas: {myDict}')
